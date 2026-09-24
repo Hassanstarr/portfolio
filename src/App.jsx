05dc407c534d73b79878a8111b8d5b3a9da1afc4
HassanStarr <hassanasif419@gmail.com>
2026-09-24 10:59:41 +0500

    Updated file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Navbar from "./sections/Navbar.jsx";
+import Navbar from "./components/Navbar.jsx";
 import Home from "./sections/Home.jsx";
 import About from "./sections/About.jsx";
 import Skills from "./sections/Skill.jsx";
 import Projects from "./sections/Project.jsx";
 import Achievements from "./sections/Achievement.jsx";
 import Contact from "./sections/Contact.jsx";
-import Footer from "./sections/Footer.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
